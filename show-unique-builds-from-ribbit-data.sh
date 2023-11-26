a0621e8b35dd709fdc0082ff97221df17e5652de
Luashine <103937213+Luashine@users.noreply.github.com>
2023-11-26 04:33:14 +0100

    Add old ribbit data filter

---
+++ show-unique-builds-from-ribbit-data.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Usage:
+# ./script <path to ribbit/ngdp data of the game you want> > version-list.txt
+
+# Example:
+# ./show-unique-builds-from-ribbit-data.sh './Warcraft-ngdp-repo' > ngdp-repo-20221013-AllWar3.cfg
+
+ribbitPath="$1"
+
+function extractBuildConfigs() {
+	local ribbitFolder="$1"
+	
+	local prevBuildConfig=""
+	
+	# https://stackoverflow.com/questions/8677546/reading-null-delimited-strings-through-a-bash-loop
+	while IFS= read -r -d $'\0' file; do
+		# Arbitrary operations on "$file" here
+		local fileName="$(basename "$file")"
+		
+		# header _could_ be dynamic
+		local buildDefinition="$(grep -F 'Region!' "$file" --after-context 2 | tail -n 1)"
+		#echo "${fileName}: $buildDefinition"
+		
+		local buildConfig="$(echo "$buildDefinition" | cut -d '|' -f 2)"
+		# dont print duplicates, but files are out of order anyway
+		if [[ "$buildConfig" != "$prevBuildConfig" ]]; then
+			local versionName="$(echo "$buildDefinition" | cut -d '|' -f 6)"
+			
+			# 1.32.2.14722|fd283a3545d954fa86e36a1e464fd226|version-w3-152708.bmime
+			echo "${versionName}|${buildConfig}|${fileName}"
+		fi;
+		
+		prevBuildConfig="$buildConfig"
+		
+	done < <(find "$ribbitFolder" -iname 'version*' -type f -print0)
+
+	unset IFS;
+}
+
+function uniqueBuildConfigs() {
+	local ribbitFolder="$1"
+	local prevBuildConfig=""
+	
+	# sort by buildconfig hash then dedupe here
+	while read -r configLine; do
+		local buildConfig="$(echo "$configLine" | cut -d '|' -f 2)"
+		
+		if [[ "$buildConfig" != "$prevBuildConfig" ]]; then
+			echo "$configLine"
+		fi
+		
+		prevBuildConfig="$buildConfig"
+		
+	done < <(extractBuildConfigs "$ribbitFolder" | sort -t '|' -k 2)
+}
+
+>&2 echo "Please wait! The data is being extracted, sorted, then deduplicated"
+uniqueBuildConfigs "$ribbitPath" | sort --version-sort
